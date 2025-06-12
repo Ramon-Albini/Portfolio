777ad4088138e96e6f96d1c87c30143451a8fc9f
Ramon-Albini <166454486+Ramon-Albini@users.noreply.github.com>
2025-06-12 19:52:52 -0300

    Delete SpringProject/teste_spring directory

--- SpringProject/teste_spring/src/main/java/com/esoft/teste_spring/Exceptions/NaoEncontradoException.java
+++
@@ -1,9 +0,0 @@
-package com.esoft.teste_spring.Exceptions;
-
-public class NaoEncontradoException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-	public NaoEncontradoException(String mensagem) {
-        super(mensagem);
-    }
-}
